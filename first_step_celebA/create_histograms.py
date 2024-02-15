503b1ce983aa343e944236bf4a2ef20886e1d64c
maximilian_rieger <mr@nxrt.io>
2024-02-15 14:00:33 +0100

    updated histogram creation script for celebA data

--- first_step_celebA/create_histograms.py
+++ first_step_celebA/create_histograms.py
@@ -1,30 +1,32 @@
 # create the histograms for the cifar10 dataset
 
-from datasets.load_cifar10_samples import load_cifar10_samples
+from datasets.load_celebA_samples import load_celebA_samples
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import tqdm
 
 # create rgb histogram
 def create_rgb_histograms(data):
     histograms = []
-    for img in data:
-        red_hist = np.histogram(img[0:1024], bins=256, range=[0, 256])[0]
-        green_hist = np.histogram(img[1024:2048], bins=256, range=[0, 256])[0]
-        blue_hist = np.histogram(img[2048:], bins=256, range=[0, 256])[0]
+    for img_path in tqdm.tqdm(data, desc='Creating histograms', unit='image'):
+        img = plt.imread(img_path)
+        red_hist = np.histogram(img[:,:,0].ravel(), bins=256, range=[0, 256])[0]
+        green_hist = np.histogram(img[:,:,1].ravel(), bins=256, range=[0, 256])[0]
+        blue_hist = np.histogram(img[:,:,2].ravel(), bins=256, range=[0, 256])[0]
         histograms.append(np.concatenate((red_hist, green_hist, blue_hist)))
     return np.array(histograms)
 
 def label2string(label):
-    labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    labels = ['Smiling', 'Blond_Hair', 'Black_Hair', 'Brown_Hair', 'Gray_Hair', 'Pale_Skin', 'Narrow_Eyes', 'Rosy_Cheeks', 'Eyeglasses', 'Wearing_Lipstick']
     return labels[label]
 
 if __name__ == '__main__':
     # check if the histograms are already created and load them
-    if not os.path.exists(os.path.join(os.getcwd(), 'train_hist.npy')):
+    # if not os.path.exists(os.path.join(os.getcwd(), 'train_hist.npy')):
+    if True:
         print('Creating histograms...')
-        # load the cifar10 dataset
-        train_data, train_labels, test_data, test_labels = load_cifar10_samples()
+        # load the celebA dataset
+        train_data, train_labels, test_data, test_labels = load_celebA_samples()
         # create the histograms
         train_hist = create_rgb_histograms(train_data)
         test_hist = create_rgb_histograms(test_data)
@@ -57,8 +59,11 @@ if __name__ == '__main__':
     # plot a figure with the histograms of the first 10 images
     plt.figure()
     plt.title('Histograms of the first 10 images')
-    for i in range(10):
-        plt.plot(train_hist[i], label=label2string(train_labels[i]))
+    hists = train_hist[::5000]
+    labels = train_labels[::5000]
+    for i, data in enumerate(zip(hists, labels)):
+        hist, label = data
+        plt.plot(hist, label=label2string(label))
         # add the axis labels
         plt.xlabel('Bins')
         plt.ylabel('Frequency')
