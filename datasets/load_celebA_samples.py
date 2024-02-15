c0fb8e6fd1ca45f9cde62169768dd77364787d3a
maximilian_rieger <mr@nxrt.io>
2024-02-15 13:29:32 +0100

    updated load celebA script, added full path for easier retrieval.

--- datasets/load_celebA_samples.py
+++ datasets/load_celebA_samples.py
@@ -20,14 +20,18 @@ def load_celebA_samples():
     train_img_names = []
     for attribute in attributes:
         train_img_names += list(attr_df[attribute].values)
-    train_img_names = np.array(train_img_names).astype(object)
-    # add the directory to the image names
+    train_img_names = np.array(train_img_names).astype(str)
     # create the train_labels
     train_labels = np.zeros(train_img_names.shape[0])
     for i, attribute in enumerate(attributes):
         # train_labels[np.where(attr_df[attribute].isin(train_img_names))] = i
         train_labels[np.where(attr_df[attribute].isin(train_img_names))[0] + i * 5000] = i
     train_labels = train_labels.astype(int)
+    # add the directory to the image names
+    directory = np.zeros(train_img_names.shape[0]).astype(object)
+    directory.fill(os.path.join(data_dir, 'img_align_celeba', ''))
+    directory = directory.astype(str)
+    train_img_names = np.char.add(directory, train_img_names)
     # load the celebA_chosen_attributes_test.csv file
     attr_file = os.path.join(data_dir, 'celebA_chosen_attributes_test.csv')
     attr_df = pd.read_csv(attr_file, sep=',', header=0)
@@ -37,13 +41,18 @@ def load_celebA_samples():
     test_img_names = []
     for attribute in attributes:
         test_img_names += list(attr_df[attribute].values)
-    test_img_names = np.array(test_img_names)
+    test_img_names = np.array(test_img_names).astype(str)
     # create the test_labels
     test_labels = np.zeros(test_img_names.shape[0])
     for i, attribute in enumerate(attributes):
         # test_labels[np.where(attr_df[attribute].isin(test_img_names) )] = i
         test_labels[np.where(attr_df[attribute].isin(test_img_names))[0] + i * 1000] = i
     test_labels = test_labels.astype(int)
+    # add the directory to the image names
+    directory = np.zeros(test_img_names.shape[0]).astype(object)
+    directory.fill(os.path.join(data_dir, 'img_align_celeba', ''))
+    directory = directory.astype(str)
+    test_img_names = np.char.add(directory, test_img_names)
     os.chdir(prvs_dir)
     return train_img_names, train_labels, test_img_names, test_labels
 
@@ -62,7 +71,7 @@ if __name__ == '__main__':
     labels = train_labels[::5000]
     for i, data in enumerate(zip(imgs, labels)):
         img, label = data
-        img = plt.imread(os.path.join(data_dir, 'img_align_celeba', img))
+        img = plt.imread(img)
         plt.subplot(2, 5, i + 1)
         plt.imshow(img)
         plt.title(f'Class: {label}')
